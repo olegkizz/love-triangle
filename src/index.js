6b61b288261bfc7fc23d1db5abede7b228e9cea0
olegkizz <olegkizz1998@gmail.com>
2018-02-21 18:02:46 +0300

    Created Task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  	for (var i = 0;i<preferences.length;++i)
+  	{
+  		if (i == preferences[preferences[preferences[i]-1]-1]-1 && i != preferences[i]-1)
+  			{
+  				count ++;
+  				preferences[i] = 0;
+  			}
+  	}
+  	return count;
 };
